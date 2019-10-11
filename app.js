d1a1a1d15bf720d7aa64d9077bfa20e9c7fa6a0d
Nsuliman <nsuliman88@gmail.com>
2019-10-11 17:49:23 +0300

    solved 6th problem

--- app.js
+++ app.js
@@ -10,22 +10,24 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 
-
 var global = 0 ; /// to save globaly the result value of Sum and multiby
 
 function sum(a, b) { //eslint-disable-line
 // num and string 
 
 var Result = a+b;
-var string = ' The sum of  ' + a + '  and  ' + b + ' is ' + Result + '.' ; 
+var string ='The sum of '+a+' and '+b+' is '+Result+'.' ; 
+
 //console.log(' Result Of Sum Two Numbers  = ' + Result);
 global = Result ; 
-return [ Result ,string] ; 
+
+//console.log(SumArry);
+return [Result,string]; 
 }
 
 
 // Here is the test for sum(); uncomment it to run it
-//testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -43,14 +45,17 @@ Test this function by hand in the console to get it working, and when you think 
 function multiply(a,b) { //eslint-disable-line
 
     var Result = a*b ;
-    var string = ' The product of  ' + a + '  and  ' + b + ' is  ' + Result + '.' ;
+    var string ='The product of '+a+' and '+b+' is '+Result+'.' ; 
+
     global = Result ; 
+    // globalPro = b + Result;
+
    // console.log(' Result Of Product Two Numbers  = ' + Result);
     return [ Result ,string] ; 
 }
 
 // Here is the test for multiply(); uncomment it to run it
- //testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -74,19 +79,23 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
 
+    var Sum3NumPro = [];
+
     var ResultofSum2Num = sum(a,b) ;
-    var ResultofSum3Num = sum(global,c);
-    var string1 = a + ' and  ' + b + '  and  ' + c  + ' sum to  ' + ResultofSum3Num + '.' ; 
-
+    var ResultofSum3Num = global +c ;
+    var string1 = a+ ' and ' + b +' and ' + c + ' sum to ' + ResultofSum3Num+'.' ; 
+    
     var ResultofPro2Num = multiply(a,b) ;
-    var ResultofPro3Num = multiply(global,c);
-    var string2 = ' The product of ' + a + '  and  ' + b + ' and  ' + c + ' is ' + ResultofPro3Num + '.' ; 
+    var ResultofPro3Num = global * c;
+    var string2 = 'The product of ' +a+ ' and ' + b +' and ' + c + ' is ' + ResultofPro3Num+'.' ; 
 
-    return [  ' Result of sum Three Numbers = ' + ResultofSum3Num ,' Result of Multiply Three Numbers = ' + ResultofPro3Num , string1 , string2] ; 
+    // var string2 = ' The product of ' + a + '  and  ' + b + ' and  ' + c + ' is ' + ResultofPro3Num + '.' ; 
+
+    return [ResultofSum3Num,ResultofPro3Num,string1,string2] ; 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -109,18 +118,18 @@ var testArray = [2, 3, 4]; //eslint-disable-line
 function sumArray(sumArr) { //eslint-disable-line
 
     var ResultofSum2NumArry = sum(testArray[0],testArray[1]) ;
-    var ResultofSum3NumArry = sum(global,testArray[2]);
+    var ResultofSum3NumArry =global+testArray[2];
 
     //console.log('sum of two array num = ' + ResultofSum2NumArry + 'sum of three array num = ' + ResultofSum3NumArry );
 
-    var string = '2,3,4 was passed in as an array of numbers , and ' + ResultofSum3NumArry + ' is their sum.'
+    var string = '2,3,4 was passed in as an array of numbers, and '+ ResultofSum3NumArry + ' is their sum.'
     return [ ResultofSum3NumArry ,string] ; 
 
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -138,16 +147,16 @@ Test this function by hand in the console to get it working, and when you think 
 function multiplyArray(multArr) { //eslint-disable-line
 
     var ResultofPro2NumArry = multiply(testArray[0],testArray[1]) ;
-    var ResultofPro3NumArry = multiply(global,testArray[2]);
+    var ResultofPro3NumArry = global * testArray[2];
 
     // console.log('product of two array num = ' + ResultofPro2NumArry + 'product of three array num = ' + ResultofPro3NumArry );
 
-    var string = '2,3,4 was passed in as an array of numbers , and ' + ResultofPro3NumArry + ' is their sum.'
+    var string = 'The numbers 2,3,4 have a product of ' + ResultofPro3NumArry + '.'
     return [ ResultofPro3NumArry ,string] ;
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
@@ -178,12 +187,12 @@ function multiplyAnyArray(dynamicArray) { //eslint-disable-line
     {   
         SumOfProArray = SumOfProArray * testDynamicArray[i];
     }
-    var string = 'The numbers 1,2,3,4,5 have a product of  ' + SumOfProArray + '.' ;
+    var string = 'The numbers 1,2,3,4,5 have a product of ' + SumOfProArray + '.' ;
 
     return [SumOfProArray,string];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
