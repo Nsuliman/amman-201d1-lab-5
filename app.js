576ff77e6a37bb408afa189ba90e58fc0e5dcabe
Nsuliman <nsuliman88@gmail.com>
2019-10-11 14:19:27 +0300

    solved 3rd problem

--- app.js
+++ app.js
@@ -5,15 +5,21 @@ Write a function called sum() that takes in two numbers as arguments and then re
 
 "The sum of 4 and 7 is 11."
 
+
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
+
+
+var global = 0 ;
+
 function sum(a, b) { //eslint-disable-line
 // num and string 
 
 var Result = a+b;
 var string = ' The sum of  ' + a + '  and  ' + b + ' is ' + Result + '.' ; 
-console.log(' Result Of Sum  = ' + Result);
+console.log(' Result Of Sum Two Numbers  = ' + Result);
+global = Result ; 
 return [ Result ,string] ; 
 }
 
@@ -38,7 +44,8 @@ function multiply(a,b) { //eslint-disable-line
 
     var Result = a*b ;
     var string = ' The product of  ' + a + '  and  ' + b + ' is  ' + Result + '.' ;
-    console.log(' Result Of Product  = ' + Result);
+    global = Result ; 
+    console.log(' Result Of Product Two Numbers  = ' + Result);
     return [ Result ,string] ; 
 }
 
@@ -57,21 +64,25 @@ where the first element is the sum of those three numbers, the second element is
 Third element: "4 and 7 and 5 sum to 16."
 Fourth element: "The product of 4 and 7 and 5 is 140."
 
-IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, use your sum() function, and to do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
+IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, 
+use your sum() function, and to do multiplication, use your multiply() function that you've already created. 
+You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + 
+operator for string concatenation.
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
 
-    var Result1 = a+b+c ;
-    var Result2 = a*b*c ;
+    var ResultofSum2Num = sum(a,b) ;
+    var ResultofSum3Num = sum(global,c);
+    var string1 = a + ' and  ' + b + '  and  ' + c  + ' sum to  ' + ResultofSum3Num + '.' ; 
 
-    var string1 = a + ' and  ' + b + '  and  ' + c  + ' sum to  ' + Result1 + '.' ; 
-    var string2 = ' The product of ' + a + '  and  ' + b + ' and  ' + c + ' is ' + Result1 + '.' ; 
+    var ResultofPro2Num = multiply(a,b) ;
+    var ResultofPro3Num = multiply(global,c);
+    var string2 = ' The product of ' + a + '  and  ' + b + ' and  ' + c + ' is ' + ResultofPro3Num + '.' ; 
 
-    console.log(' Result Of Sum & Product  = ' + Result1 ,Result2);
-    return [  ' Result of sum = ' + Result1 ,' Result of Multiply = ' + Result2 , string1 , string2] ; 
+    return [  ' Result of sum Three Numbers = ' + ResultofSum3Num ,' Result of Multiply Three Numbers = ' + ResultofPro3Num , string1 , string2] ; 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
@@ -81,7 +92,10 @@ function sumAndMultiply(a, b, c) { //eslint-disable-line
 
 /////////////////////////////////////
 /* Problem 4
-Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+Write a function called sumArray() that takes in an array of numbers as its single argument 
+and then returns an array where the first element is the sum of the numbers in the array, 
+and the second element is a string that EXACTLY follows this example and uses the values 
+that were input into the function:
 
 "2,3,4 was passed in as an array of numbers, and 9 is their sum."
 
@@ -94,6 +108,7 @@ var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
 
+    var string = '2,3,4 was passed in as an array of numbers , and ' + sumArr + ' is their sum.'
 }
 
 // Here is the test for sumArray(); uncomment it to run it
